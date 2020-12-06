35f69ff4f91d3db3edba93d9ce55e1e92a5b3b1d
Leonardo Guilhoto <leonardoguilhoto@uchicago.edu>
2020-12-05 23:19:59 -0500

    Wrote code for fixed iteration, but think my get_phi is wrong

--- beta_model.py
+++ beta_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_beta(n, low=-10, high=10):
+def generate_beta(n, low=-1, high=1):
     '''
     Generates an n-dimensional beta array where components are chosen
     uniformly in the interval [low, high]
@@ -15,7 +15,7 @@ def generate_graph(beta):
     beta-model. Returns the adjecency matrix of the graph.
     '''
     n = len(beta)
-    adj_matix = np.zeros((n,n))
+    adj_matrix = np.zeros((n,n))
     for i in range(n):
         for j in range(i):
             factor = np.exp(beta[i]+beta[j])
@@ -24,4 +24,36 @@ def generate_graph(beta):
                 adj_matrix[j,i] = 1
     return adj_matrix
 
+
+def get_phi(adj_matrix):
+    '''
+    Given an adjecency matrix for a graph in the beta-model, returns
+    the function phi of that graph to be used in fixed point
+    iteration.
+    '''
+    n = len(adj_matrix)
+    deg_seq = np.zeros(n)
+    for i in range(n):
+        for j in range(n):
+            deg_seq[i] += adj_matrix[i,j]
+    def phi(x):
+        phi_x = np.empty(n)
+        for i in range(n):
+            sums = 0
+            for j in range(n):
+                if i != j:
+                    sums += 1/(np.exp(x[i])+np.exp(-x[j]))
+            phi_x[i] = np.log(deg_seq[i])-np.log(sums)
+        return phi_x #FIXME what if a node has degree 0?
+    return phi
+
+def fixed_iter(phi, x_0, n=10**2):
+    '''
+    Given a function phi and an initial guess x_0, computes n steps
+    of fixed iteration and returns the new result.
+    '''
+    x = x_0
+    for _ in range(n):
+        x = phi(x)
+    return x
 
